6dfb6089cba30f51889a1ccca8bac36e11058987
Vivian Allen <vivian.allen@methods.co.uk>
2020-10-02 13:53:22 +0100

    build dependencies in lambda docker

--- requirements_2_lambda_pkg_layer.sh
+++ requirements_2_lambda_pkg_layer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "Removing previous python_pkgs_lambda_layer.zip file if it exists..."
 rm python_pkgs_lambda_layer.zip
 
@@ -9,8 +8,15 @@ WORKING_DIR=$(mktemp -d)
 PKG_DIR="$WORKING_DIR"/python
 mkdir "$PKG_DIR"
 
-echo "Pip installing requirements.txt to temp pkg directory..."
-pip install -r requirements.txt -t "$PKG_DIR"
+echo "entering docker"
+
+
+echo "Pip installing requirements.txt (in docker) to temp pkg directory..."
+docker run \
+    -v ${CURRENT_DIR}:/requirements_dir \
+    -v ${PKG_DIR}:/pkg_dir \
+    "lambci/lambda:build-python3.6" \
+    /bin/sh -c "pip install -r /requirements_dir/requirements.txt -t /pkg_dir; exit"
 
 echo "Zipping pkg directory to python_pkgs_lambda_layer.zip..."
 cd "$WORKING_DIR" && zip -r "$CURRENT_DIR"/python_pkgs_lambda_layer .
